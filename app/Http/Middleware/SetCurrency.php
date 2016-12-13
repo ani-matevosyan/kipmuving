d8e3ee3fc552adfefb08c9bef31e5176f7bef7a9
age007nt <sanek.solodovnikov.94@gmail.com>
2016-12-14 00:18:58 +0100

    Currencies update

--- app/Http/Middleware/SetCurrency.php
+++ app/Http/Middleware/SetCurrency.php
@@ -15,17 +15,44 @@ class setCurrency
 	
 	public function handle($request, Closure $next)
 	{
-//		$currency = $this->currency->where('code', 'USDCLP')->first();
-//		dd(Carbon::now()->subMinutes(1)->toDateTimeString(), Carbon::now()->subMinutes(1)->toDateTimeString() > Carbon::parse($currency['updated_at'])->toDateTimeString(), Carbon::parse($currency['updated_at'])->toDateTimeString());
-//		if (Carbon::now()->subMinutes(1)->toDateTimeString() > Carbon::parse($currency['updated_at'])->toDateTimeString()) {
-//			$url = 'http://apilayer.net/api/live?access_key=390417d6045e37cadb7b22ad884ca4df&currencies=CLP&format=1';
-//			$json = json_decode(file_get_contents($url), true);
-//			$currency->value = $json['quotes']['USDCLP'];
-//			$currency->save();
-//		} else
-//			dd('<');
-//
-//		dd(Carbon::now()->subMinutes(1)->toDateTimeString(), Carbon::parse($currency['updated_at'])->toDateTimeString());
+		$time = 60; //Minutes to update currency
+		$currency = $this->currency->where('code', 'USDCLP')->first();
+		
+		if (session('currency')) {
+			if (Carbon::parse(session('currency.updated_at'))->addMinutes($time) < Carbon::now()) {
+				#If data in DB need to update currency
+				if (Carbon::now()->subMinutes($time) > Carbon::parse($currency['updated_at'])) {
+					#Get currency
+					#TODO change link
+					$url = 'http://apilayer.net/api/live?access_key=390417d6045e37cadb7b22ad884ca4df&currencies=CLP&format=1';
+					$json = json_decode(file_get_contents($url), true);
+					
+					#Update currency in DB
+					$currency->value = $json['quotes']['USDCLP'];
+					$currency->updated_at = Carbon::now()->toDateTimeString();
+					$currency->save();
+					
+					#Collect updated data
+					$data = [
+						'value'      => $json['quotes']['USDCLP'],
+						'updated_at' => Carbon::now()->toDateTimeString()
+					];
+				} else {
+					#Collect old currency value and update time
+					$data = [
+						'value'      => $currency['value'],
+						'updated_at' => Carbon::now()->toDateTimeString()
+					];
+				}
+				session()->put('currency', $data);
+			}
+		} else {
+			$data = [
+				'value'      => $currency['value'],
+				'updated_at' => Carbon::now()->toDateTimeString()
+			];
+			session()->put('currency', $data);
+		}
 		
 		return $next($request);
 	}
