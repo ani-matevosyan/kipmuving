fe17b64ca4c9957202cada898d8dcb878c94d699
age007nt <sanek.solodovnikov.94@gmail.com>
2017-01-03 19:09:56 +0100

    Delete persons from offers (admin)

---
+++ database/migrations/2017_01_03_123111_add_available_time_field_to_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvailableTimeFieldToOffersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('offers', function (Blueprint $table) {
+			//
+			$table->text('available_time')->nullable();
+		});
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('offers', function (Blueprint $table) {
+			//
+			$table->removeColumn('available_time');
+		});
+	}
+}
