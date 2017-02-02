a8ef59e0b4d840aee677d07b1c7793b596209993
Falcornus <zvipromus@i.ua>
2017-02-02 01:54:43 +0100

    Pack of fixes

--- public/js/product.tour.js
+++ public/js/product.tour.js
@@ -24,18 +24,18 @@ $(window).load(function(){
         });
         productTour_home.steps([{
             element: '#activity-form',
-            title: 'Activity form',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Busque su actividad',
+            content: 'Busque las actividades que quiere hacer y seleccione la fecha que estará en Pucón.',
             image: 'images/tour/home-tour-1.jpg'
         }, {
             element: '#program-schedule',
-            title: 'Program schedule',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Su carrito de actividades',
+            content: 'Aquí estará su carrito de actividades. Para ir finalizando su reserva, presione AGENDA.',
             image: 'images/tour/home-tour-2.jpg'
         }, {
             element: '#guia',
-            title: 'Guia section',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Guia de Pucón',
+            content: 'Preparamos una guia completa de Pucón con las actividades más buscadas y también tours gratuitos que puede hacer.',
             image: 'images/tour/home-tour-3.jpg'
         }]);
         productTour_home.startTour();
@@ -48,15 +48,21 @@ $(window).load(function(){
         });
         productTour_activities.steps([{
             element: '#first-slider-sec',
-            title: 'Slider',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Los más pedidos',
+            content: 'Aquí puedes encontrar cuales son las actividades más buscadas en Pucón.',
             image: 'images/tour/activities-tour-1.jpg'
         }, {
             element: '#floatdiv',
-            title: 'Types',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Estilos de Actividades',
+            content: 'Para facilitar, separamos las actividades por estilos.',
             image: 'images/tour/activities-tour-2.jpg'
-        }]);
+        },{
+            element: '#activities-info',
+            title: 'Iconografia',
+            content: 'Algunas clasificaciones de actividades, que te ayudará a elegir el momento correcto de hacerlas.',
+            image: 'images/tour/activities-tour-3.jpg'
+        }
+        ]);
         productTour_activities.startTour();
         localStorage.activitiestour = "visited";
     }
@@ -67,24 +73,29 @@ $(window).load(function(){
         });
         productTour_activity.steps([{
             element: '.jcf-select-persona',
-            title: 'Persons',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            title: 'Elija la cantidad de Personas',
+            content: 'Para cada actividad, seleccione la cantidad de personas',
             image: '../images/tour/activity-tour-1.jpg'
         }, {
-            element: '.btn-reserve',
-            title: 'Reserve',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
+            element: '.jcf-select-hours',
+            title: 'Los horarios',
+            content: 'Algunas actividades poseen dos horarios distintos, elija aquel que más le acomode',
             image: '../images/tour/activity-tour-2.jpg'
         }, {
+            element: '.btn-reserve',
+            title: 'La fecha',
+            content: 'Recuerde de seleccionar la fecha correcta para la actividad que quiere hacer',
+            image: '../images/tour/activity-tour-3.jpg'
+        }, {
             element: '#program-schedule',
-            title: 'Program schedule',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
-            image: '../images/tour/home-tour-2.jpg'
+            title: 'Incluya la actividad',
+            content: 'Presione el botón AGREGAR, para incluir en su carrito de actividades',
+            image: '../images/tour/activity-tour-4.jpg'
         }, {
             element: '#reserve-date',
-            title: 'Reserve date',
-            content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
-            image: '../images/tour/activity-tour-4.jpg'
+            title: 'Su carrito',
+            content: 'Luego presionar AGREGAR, sus actividades estarán en su carrito. Para finalizar su reserva, presione MI AGENDA',
+            image: '../images/tour/home-tour-2.jpg'
         }
         ]);
         productTour_activity.startTour();
