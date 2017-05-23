023ccc5a7c55846a8f4179dd84325f298b5c79ff
Falcornus <zvipromus@i.ua>
2017-05-23 12:53:40 +0200

    Payment update

--- public/js/custom.js
+++ public/js/custom.js
@@ -171,6 +171,29 @@ $(document).ready(function () {
                 }, 3000)
             }
         })
-    })
+    });
+
+
+    $('.payu-btn').click(function (event) {
+        event.preventDefault();
+        var thisBtn = $(this);
+        thisBtn.attr('disabled', true);
+        $.ajax({
+            type: "GET",
+            url: "/reserve/payu",
+            data: {
+                '_token': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function (data) {
+                for (key in data) {
+                    if (data.hasOwnProperty(key)) {
+                        $('form[name=payuform]>input[name=' + key + ']').val(data[key]);
+                    }
+                }
+                thisBtn.attr('disabled', false);
+                document.payuform.submit();
+            }
+        })
+    });
 
 });
--- resources/views/site/reservar/su-reservar.blade.php
+++ resources/views/site/reservar/su-reservar.blade.php
@@ -303,31 +303,6 @@
 					{{--<input name="sUrl" type="hidden" value="">--}}
 					{{--<input name="fUrl" type="hidden" value="">--}}
 				</form>
-				<script>
-            $(document).ready(function () {
-                $('.payu-btn').click(function (event) {
-                    event.preventDefault();
-                    var thisBtn = $(this);
-                    thisBtn.attr('disabled', true);
-                    $.ajax({
-                        type: "GET",
-                        url: "/reserve/payu",
-                        data: {
-                            '_token': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        success: function (data) {
-                            for (key in data) {
-                                if (data.hasOwnProperty(key)) {
-                                    $('form[name=payuform]>input[name=' + key + ']').val(data[key]);
-                                }
-                            }
-                            thisBtn.attr('disabled', false);
-                            document.payuform.submit();
-                        }
-                    })
-                });
-            });
-				</script>
 			</div>
 		</div>
 	</div>
