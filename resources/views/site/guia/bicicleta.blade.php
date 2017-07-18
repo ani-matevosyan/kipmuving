de3055551c532ef73e991fd84620dcf02ec9def5
Falcornus <zvipromus@i.ua>
2017-07-18 14:09:01 +0300

    bicycle page update

--- resources/views/site/guia/bicicleta.blade.php
+++ resources/views/site/guia/bicicleta.blade.php
@@ -49,6 +49,120 @@
         }
     </script>
 
+
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.38.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.38.0/mapbox-gl.css' rel='stylesheet' />
+    <div id='map'></div>
+    <script>
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZmFsY29ybnVzIiwiYSI6ImNqNHI0MHRrajBodWwzM3BjN2RhenJ1MnQifQ.tUfKaBZecMpeZ6vYh1QKyQ';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [-71.969,-39.275],
+            zoom: 16.5
+        });
+
+        map.on('load', function () {
+
+
+            map.addSource("bicycle-route",{
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": [
+                                [
+                                    -71.97168394357989,
+                                    -39.27540274218929
+                                ],
+                                [
+                                    -71.96968540399888,
+                                    -39.274812746394296
+                                ],
+                                [
+                                    -71.96952450462979,
+                                    -39.27479963531452
+                                ],
+                                [
+                                    -71.9691349587763,
+                                    -39.27495041251989
+                                ],
+                                [
+                                    -71.96882162842064,
+                                    -39.2751274114309
+                                ],
+                                [
+                                    -71.968508298065,
+                                    -39.27480619086175
+                                ],
+                                [
+                                    -71.96864379226817,
+                                    -39.27470785772992
+                                ],
+                                [
+                                    -71.96813568898577,
+                                    -39.274294857097644
+                                ]
+                            ]
+                        }
+                    },{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                -71.97168394357989,
+                                -39.27540274218929
+                            ]
+                        }
+                    },{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                -71.96813568898577,
+                                -39.274294857097644
+                            ]
+                        }
+                    }]
+                }
+            });
+
+            map.addLayer({
+                "id": "route",
+                "type": "line",
+                "source": "bicycle-route",
+                "layout": {
+                    "line-join": "round",
+                    "line-cap": "round"
+                },
+                "paint": {
+                    "line-color": "#73B2DF",
+                    "line-width": 5,
+                    "line-opacity": 0.8
+                }
+            });
+            map.addLayer({
+                "id": "points",
+                "type": "circles",
+                "source": "bicycle-route",
+                "paint": {
+                    "circle-radius": 6,
+                    "circle-color": "#B42222"
+                }
+            });
+        });
+    </script>
+
+    <style>
+        #map{
+            height: 500px;
+            width: 100%;
+        }
+    </style>
+
 </div>
 
 @stop
