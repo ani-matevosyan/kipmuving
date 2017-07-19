1138a17d037005930cfe18066a6552eedf01ef26
age007nt <sanek.solodovnikov.94@gmail.com>
2017-07-19 12:27:46 +0300

    Free activities fix #2

--- app/Admin/GuideActivity.php
+++ app/Admin/GuideActivity.php
@@ -32,7 +32,7 @@ AdminSection::registerModel(GuideActivity::class, function (ModelConfiguration $
 		return $display;
 	});
 	
-	$model->onCreateAndEdit(function ($id) {
+	$model->onEdit(function ($id) {
 		
 		$activity = GuideActivity::find($id);
 		
@@ -118,6 +118,89 @@ AdminSection::registerModel(GuideActivity::class, function (ModelConfiguration $
 				'Bus data'       => $bus_data
 			]);
 		}
+		
+		$form->addElement($tabs);
+		
+		return $form;
+	});
+	
+	$model->onCreate(function () {
+	
+		$form = AdminForm::panel()->setHtmlAttribute('enctype', 'multipart/form-data');
+		
+		$warning_bus_est_time = '<div class="alert bg-warning text-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><small><strong>For example:</strong><br>120</small></div>';
+		$warning_bus_est_exp = '<div class="alert bg-warning text-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><small><strong>For example:</strong><br>1500</small></div>';
+		
+		$guide_activity = new \SleepingOwl\Admin\Form\FormElements([
+			AdminFormElement::columns()
+				->addColumn([
+					AdminFormElement::text('name', 'Name')->required()
+				], 6)
+				->addColumn([
+					AdminFormElement::select('category', 'Category')->required()->setOptions([
+						'Visual'    => 'Visual',
+						'Caminatas' => 'Caminatas',
+						'Termas'    => 'Termas'
+					])
+				], 2)
+				->addColumn([
+					AdminFormElement::select('page', 'PAGE')->required()->setOptions([
+						'bus'      => 'By car or bus',
+						'cultural' => 'Cultural tour'
+					])
+				], 2)
+				->addColumn([
+					AdminFormElement::select('region', 'REGION')->required()->setOptions([
+						'pucon'   => 'Pucon',
+						'atacama' => 'Atacama'
+					])
+				], 2),
+			AdminFormElement::columns()
+				->addColumn([
+					AdminFormElement::text('instagram_id', 'Instagram')->required()
+				], 4)
+				->addColumn([
+					AdminFormElement::text('latitude', 'Latitude')->required()
+				], 4)
+				->addColumn([
+					AdminFormElement::text('longitude', 'Longitude')->required()
+				], 4),
+			AdminFormElement::text('short_description', 'Short description')->required(),
+			AdminFormElement::textarea('description', 'Description')->required(),
+			AdminFormElement::columns()
+//					->addColumn([
+//						AdminFormElement::textarea('real_time_ranges', 'Time')->required(),
+//					], 8)
+				->addColumn([
+					AdminFormElement::image('image', 'Image'),
+				], 12),
+		]);
+		
+		$tripadvisor = new \SleepingOwl\Admin\Form\FormElements([
+			AdminFormElement::textarea('tripadvisor_code', 'Tripadvisor code'),
+		]);
+		
+		$bus_data = new \SleepingOwl\Admin\Form\FormElements([
+			AdminFormElement::columns()
+				->addColumn([
+					AdminFormElement::html($warning_bus_est_time),
+					AdminFormElement::text('real_bus_est_time', 'Est. time (in minutes)'),
+				], 6)
+				->addColumn([
+					AdminFormElement::html($warning_bus_est_exp),
+					AdminFormElement::text('bus_est_expenditure', 'Est. expenditure (per person)'),
+				], 6),
+//					->addColumn([
+//						AdminFormElement::text('bus_est_service', 'Est. service (per person)'),
+//					], 4),
+			AdminFormElement::textarea('bus_description', 'Description'),
+		]);
+		
+			$tabs = AdminDisplay::tabbed([
+				'Guide activity' => $guide_activity,
+				'Tripadvisor'    => $tripadvisor,
+				'Bus data'       => $bus_data
+			]);
 		
 		$form->addElement($tabs);
 		
