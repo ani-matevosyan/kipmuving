198de5282ce27dd11277ac3cc98a805632a717d1
age007nt <sanek.solodovnikov.94@gmail.com>
2017-08-14 15:29:16 +0300

    Translate guide pages

--- resources/views/site/guide/shops-and-services.blade.php
+++ resources/views/site/guide/shops-and-services.blade.php
@@ -1,14 +1,14 @@
 @extends('site.guide.layout')
 
-{{-- Subpage --}}
 @section('subpage')
 
-    <section class="guide-content">
-        <h1 class="guide-content__title">{{ trans('main.shops_and_services') }}</h1>
-        <p class="guide-content__paragraph">{{ trans('main.the_main_shopping') }}<strong>Avenida O’Higgins</strong>{{ trans('main.as_well_as') }}<strong>Ansorena, Fresia, Palguín, Alderete e Urrutia</strong>.</p>
-        <p class="guide-content__paragraph">{{ trans('main.besides_supermarkets') }}<strong>{{ trans('main.8_to_9') }}</strong>{{ trans('main.but_many_places') }}</p>
-        <p class="guide-content__paragraph">{{ trans('main.city_there') }}</p>
-        <img src="{{ asset('/images/guide-shops-and-services1.jpg') }}" alt="{{ trans('main.shops_and_services') }}" class="guide-content__image">
-    </section>
+  <section class="guide-content">
+    <h1 class="guide-content__title">{{ trans('guide.shops_and_services') }}</h1>
+    <p class="guide-content__paragraph">{{ trans('guide.the_main_shopping') }}<strong>Avenida O’Higgins</strong>{{ trans('guide.as_well_as') }}<strong>Ansorena, Fresia, Palguín,
+        Alderete e Urrutia</strong>.</p>
+    <p class="guide-content__paragraph">{{ trans('guide.besides_supermarkets') }}<strong>{{ trans('guide.8_to_9') }}</strong>{{ trans('guide.but_many_places') }}</p>
+    <p class="guide-content__paragraph">{{ trans('guide.city_there') }}</p>
+    <img src="{{ asset('/images/guide-shops-and-services1.jpg') }}" alt="{{ trans('guide.shops_and_services') }}" class="guide-content__image">
+  </section>
 
 @stop
--- resources/views/site/guide/transportation.blade.php
+++ resources/views/site/guide/transportation.blade.php
@@ -3,12 +3,15 @@
 {{-- Subpage --}}
 @section('subpage')
 
-    <section class="guide-content">
-        <h1 class="guide-content__title">{{ trans('main.transportation_in') }}</h1>
-        <p class="guide-content__paragraph">{{ trans('main.pucon_is_small') }}<strong>{{ trans('main.accessible_by_foot') }}</strong>{{ trans('main.with_main_streets') }}<strong>{{ trans('main.transportation_person') }}</strong>{{ trans('main.per_jorney') }}<strong>{{ trans('main.uber_also') }}</strong>{{ trans('main.use_cab') }}<strong>U$ 3</strong>{{ trans('main.for_a_ride') }}</p>
-        <p class="guide-content__paragraph">{{ trans('main.visit_near_pucon') }}<strong>{{ trans('main.villarrica') }}</strong>{{ trans('main.possible_to_use') }}<strong>{{ trans('main.minibus') }}</strong>{{ trans('main.departing_from') }}<strong>Calle Uruguay, 540</strong>{{ trans('main.in_the_center_of') }}</p>
-        <p class="guide-content__paragraph">{{ trans('main.natural_attractions') }}</p>
-        <img src="{{ asset('/images/guide-transportation1.jpg') }}" alt="{{ trans('main.transportation') }}" class="guide-content__image">
-    </section>
+  <section class="guide-content">
+    <h1 class="guide-content__title">{{ trans('guide.transportation_in') }}</h1>
+    <p class="guide-content__paragraph">{{ trans('guide.pucon_is_small') }}<strong>{{ trans('guide.accessible_by_foot') }}</strong>{{ trans('guide.with_main_streets') }}
+      <strong>{{ trans('guide.transportation_person') }}</strong>{{ trans('guide.per_jorney') }}<strong>{{ trans('guide.uber_also') }}</strong>{{ trans('guide.use_cab') }}<strong>U$
+        3</strong>{{ trans('guide.for_a_ride') }}</p>
+    <p class="guide-content__paragraph">{{ trans('guide.visit_near_pucon') }}<strong>{{ trans('guide.villarrica') }}</strong>{{ trans('guide.possible_to_use') }}
+      <strong>{{ trans('guide.minibus') }}</strong>{{ trans('guide.departing_from') }}<strong>Calle Uruguay, 540</strong>{{ trans('guide.in_the_center_of') }}</p>
+    <p class="guide-content__paragraph">{{ trans('guide.natural_attractions') }}</p>
+    <img src="{{ asset('/images/guide-transportation1.jpg') }}" alt="{{ trans('guide.transportation') }}" class="guide-content__image">
+  </section>
 
 @stop
