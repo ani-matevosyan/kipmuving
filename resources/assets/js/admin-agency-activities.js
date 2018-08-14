ea78fa6300a971d2d15350247699a095d7245df4
Ani Matevosyan <matevosyanani.h@gmail.com>
2018-08-14 22:19:13 +0400

    Admin-agency, add activity modal design

--- resources/assets/js/admin-agency-activities.js
+++ resources/assets/js/admin-agency-activities.js
@@ -1,1 +1,16 @@
 require('./common');
+window.select2 = require('select2');
+
+
+$(document).ready(function(){
+
+    $('.under-header').on("click", "button", function () {
+        $('#addActivityModal').modal('show');
+    });
+
+    $('.select2select').select2({
+        placeholder: '',
+        width: '100%',
+    });
+
+});
--- resources/views/site/adminAgency/activities.blade.php
+++ resources/views/site/adminAgency/activities.blade.php
@@ -161,4 +161,103 @@
             </div>
         </div>
     </div>
+    <div id="addActivityModal" class="modal fade">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Actividad</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" action="/action_page.php">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="email">Nombre</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="pwd">Valor</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="price">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3">Horario</label>
+                                    <div class="col-sm-9 times">
+                                        <div class="col-sm-4">
+                                            <input type="text" class="form-control" class="hours start_time">
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <input type="text" class="form-control" class="hours end_time">
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="operations">
+                                                <span class="glyphicon glyphicon-plus-sign"></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <input type="text" class="form-control" class="hours start_time">
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <input type="text" class="form-control" class="hours end_time">
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="operations">
+                                                <span class="glyphicon glyphicon-plus-sign"></span>
+                                                <span class="glyphicon glyphicon-remove"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="pwd">Guia</label>
+                                    <div class="col-sm-9">
+                                        <select class="select2select" multiple="multiple">
+                                            <option value="JAN">Lorem Ispum</option>
+                                            <option value="FEB">Dolar Sit Amet</option>
+                                            <option value="FEBm">Luis Lopez</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="pwd">Asistentes</label>
+                                    <div class="col-sm-9">
+                                        <select class="select2select" multiple="multiple">
+                                            <option value="JAN">Lorem Ispum</option>
+                                            <option value="DFEB">Dolar Sit Amet</option>
+                                            <option value="DFEB">Luis Lopez</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="pwd">Transportista</label>
+                                    <div class="col-sm-9">
+                                        <select class="select2select" multiple="multiple">
+                                            <option value="JAN">Lorem Ispum</option>
+                                            <option value="FEB">Dolar Sit Amet</option>
+                                            <option value="FEBm">Luis Lopez</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-sm-3" for="pwd">Minimo</label>
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" class="min">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success"> Adicionar </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
